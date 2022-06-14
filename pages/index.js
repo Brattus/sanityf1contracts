2b6899b1a9e2b07fa9ce2c86a6bb210659762938
Brattus <olemartinbratteberg@gmail.com>
2022-06-15 00:33:53 +0200

    added safelist to be safe

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,33 @@ export default function Index({ allDrivers, preview }) {
     )
   }
 
+  safelist: [
+    'grid-cols-1',
+    'grid-cols-2',
+    'grid-cols-3',
+    'grid-cols-4',
+    'grid-cols-5',
+    'grid-cols-6',
+    'grid-cols-7',
+    'grid-cols-8',
+    'grid-cols-9',
+    'grid-cols-10',
+    'grid-cols-11',
+    'grid-cols-12',
+    'col-span-1',
+    'col-span-2',
+    'col-span-3',
+    'col-span-4',
+    'col-span-5',
+    'col-span-6',
+    'col-span-7',
+    'col-span-8',
+    'col-span-9',
+    'col-span-10',
+    'col-span-11',
+    'col-span-12',
+  ],
+
   return (
     <>
       <Layout preview={preview}>
@@ -68,7 +95,6 @@ export default function Index({ allDrivers, preview }) {
     </>
   )
 }
-
 
 
 export async function getStaticProps({ preview = false }) {
