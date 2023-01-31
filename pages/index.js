29f05f9b8fa0db1f1acfb608196d43eb2b6ab7c0
Brattus <olemartinbratteberg@gmail.com>
2023-01-31 22:36:15 +0100

    fix: mobile year fix

--- pages/index.js
+++ pages/index.js
@@ -53,6 +53,33 @@ export default function Index({ allDrivers, circuits, preview }) {
   
   let circuitsSorted = circuits.sort((a, b) => a.contractEnd - b.contractEnd).reverse();
 
+  safelist: [
+    'grid-cols-1',
+    'grid-cols-2',
+    'grid-cols-3',
+    'grid-cols-4',
+    'grid-cols-5',
+    'grid-cols-6',
+    'grid-cols-7',
+    'grid-cols-8',
+    'grid-cols-9',
+    'grid-cols-10',
+    'grid-cols-11',
+    'grid-cols-12',
+    'col-span-1',
+    'col-span-2',
+    'col-span-3',
+    'col-span-4',
+    'col-span-5',
+    'col-span-6',
+    'col-span-7',
+    'col-span-8',
+    'col-span-9',
+    'col-span-10',
+    'col-span-11',
+    'col-span-12',
+  ];
+
   return (
     <>
       <Layout preview={preview}>
