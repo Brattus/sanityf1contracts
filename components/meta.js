a616c3695a4d4571e4522cf4812368aae97f6cf6
Brattus <olemartinbratteberg@gmail.com>
2023-03-21 21:46:26 +0100

    feat: changed some meta fields

--- components/meta.js
+++ components/meta.js
@@ -39,7 +39,7 @@ export default function Meta() {
 
    <title>F1 Driver and Circuit Contracts</title>
   <meta name="description" content="Get the latest information on driver and circuit contracts for the Formula One racing season. Stay up to date with all the latest signings, renewals, and negotiations in the world of F1."/>
-  <meta name="keywords" content="F1, driver contracts, circuit contracts, Formula One, racing, signings, renewals, negotiations, f1"/>
+  <meta name="keywords" content="F1, Formula 1, driver contracts, circuit contracts, Formula One, racing, signings, renewals, negotiations, f1"/>
         
       <meta property="og:image" content={'https://f1contracts.com/public/f1metaimage.webp'} />
     </Head>
--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,6 @@ export default function Index({ allDrivers, circuits, preview }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>F1 Contracts</title>
         </Head>
         <div className="flex min-h-screen flex-col justify-center bg-gray-100 py-6 sm:py-12">
           <div className="relative py-3 sm:mx-auto container">
